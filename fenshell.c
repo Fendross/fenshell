30886044719cda4eb6532d23dc77970b26c8fd48
Fendross <dario.arzuffi@icloud.com>
2025-04-22 22:59:35 +0200

    Wrote fenshell_loop and started writing fenshell_read_line.

--- fenshell.c
+++ fenshell.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+void fenshell_loop(void);
+
+char *fenshell_read_line(void);
+
+void fenshell_loop(void) {
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("$ ");
+        line = fenshell_read_line();
+        args = fenshell_split_lines(line);
+        status = fenshell_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+#define FENSHELL_RL_BUFSIZE 1024
+char *fenshell_read_line(void) {
+    int bufsize = FENSHELL_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "fenshell: buffer allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        // Read a character.
+        c = getchar();
+    }
+}
+
+int main(int argc, char **argv)
 {
+    // Load config files.
+    // TODO - No config files to load for now.
+
     // Loop Fenshell
-    printf("Hello from Fenshell\n");
-    return 0;
+    fenshell_loop();
+
+    // Shutdown and cleanup functions.
+    // TODO - No cleanup/shutdown functionalities for now.
+
+    printf("End of execution.\n");
+    return EXIT_SUCCESS;
 }
